bf53dc9134bb3f04605c70cbbace9b626fcac2b2
ssprasad-23 <shynal_007@yahoo.com>
2024-09-05 01:19:55 -0700

    create http route

--- backend/index.js
+++ backend/index.js
@@ -4,6 +4,11 @@ import {PORT} from "./config.js";
 
 const app = express()
 
+app.get('/', (request, response) => {
+    console.log(request)
+    return response.status(234).send('Welcome to my page')
+});
+
 app.listen(PORT, () => {
     console.log(`App is listening to port: ${PORT}`);
 
